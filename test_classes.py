f9c4d6460f45d44b41e5b0f635f3443707cf71cc
Grant Meng <45346532+grantmeng@users.noreply.github.com>
2020-11-10 18:33:59 -0500

    Add files via upload

---
+++ test_classes.py
@@ -0,0 +1,25 @@
+from card import Card
+from deck import Deck
+from player import Player
+from rule import SimpleRule
+import collections
+
+def test_card():
+    card = Card(2, 'red')
+    assert card.value == 2
+    assert card.color == 'red'
+
+def test_deck():
+    deck = Deck()
+    colors = collections.defaultdict(list)
+    for card in deck.cards:
+        colors[card.color].append(card.value) 
+    assert sorted(colors['red']) == list(range(10))
+    assert sorted(colors['green']) == list(range(10))
+    assert sorted(colors['yellow']) == list(range(10))
+
+def test_player():
+    p = Player('NCI')
+    assert p.name == 'NCI'
+    assert isinstance(p.rule, SimpleRule)
+    assert not p.hand
