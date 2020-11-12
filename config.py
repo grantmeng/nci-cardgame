3a38a1c272388e6c1d14f838a3476b532c6ebb81
Grant Meng <grantmeng@gmail.com>
2020-11-12 12:17:13 -0500

    added config file to make game more flexible

---
+++ config.py
@@ -0,0 +1,12 @@
+NUM_OF_PLAYER = 3
+NUM_OF_CARD = 3
+
+# each card's max value, assume min value is 0
+MAX_CARD_VALUE = 9
+
+# assume three colors with corresponding point
+COLORS = {
+    'red': 3,
+    'yellow': 2,
+    'green': 1
+}
